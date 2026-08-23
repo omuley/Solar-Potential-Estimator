e1c9473ebca167444bab1792046955e948caf31a
Ovee Muley <ovee.muley@wavvest.com>
2026-08-23 10:12:45 -0500

    test

--- server/services/rasterStorage.js
+++ server/services/rasterStorage.js
@@ -1,14 +1,14 @@
 import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
+// const s3Client = new S3Client({
+//     region: process.env.AWS_REGION,
 
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    },
-});
+//     credentials: {
+//         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+//     },
+// });
 
 export async function uploadImage(buffer, filename) {
 
